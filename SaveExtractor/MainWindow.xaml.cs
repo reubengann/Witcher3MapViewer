86a56571c3c1f50c9622d73c92cc8277bf9a1853
reubengann <reubengann@gmail.com>
2019-09-15 16:16:35 -0400

    Add convenient save extractor project

---
+++ SaveExtractor/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System.IO;
+using Witcher3MapViewer;
+using System.Windows;
+using Microsoft.Win32;
+
+namespace SaveExtractor
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        public void dumpfile(string filename)
+        {
+
+            using (FileStream compressedStream = File.OpenRead(filename))
+            using (Stream decompressedStream = ChunkedLz4File.Decompress(compressedStream))
+            using (FileStream outputstream = File.OpenWrite(filename.Substring(0, filename.Length-4) + "decomp.sav"))
+            {
+                decompressedStream.CopyTo(outputstream);
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog dlg = new OpenFileDialog
+            {
+                DefaultExt = ".sav",
+                Filter = "Save Files (*.sav)|*.sav"
+            };
+            bool? result = dlg.ShowDialog();
+            string path;
+            if (result == true)
+            {
+                path = dlg.FileName;
+                dumpfile(path);
+            }
+        }
+    }
+}
